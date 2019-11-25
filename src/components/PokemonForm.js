19fd422aec9fa92cbb1ea193177ebefad1296d3d
Nick <nboober@terpmail.umd.edu>
2019-11-25 17:32:19 -0500

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -13,13 +13,20 @@ class PokemonForm extends React.Component {
     }
   }
 
-  handleSubmit = () => {
+  handleSubmit = (event) => {
     // console.log(event.currentTarget.firstChild.firstChild)
 
     let name = document.getElementsByName("name")[0].value;
+    // let name = event.target.name.value
+
     let hp = document.getElementsByName("hp")[0].value;
+    // let hp = event.target.hp.value
+    
     let frontUrl = document.getElementsByName("frontUrl")[0].value;
+    // let frontUrl = event.target.frontUrl.value
+    
     let backUrl = document.getElementsByName("backUrl")[0].value;
+    // let backUrl = event.target.backUrl.value
 
     // console.log(name)
     // console.log(hp)
@@ -34,7 +41,7 @@ class PokemonForm extends React.Component {
     }, () => {
 
       this.props.addPokemon(this.state.name, this.state.hp, this.state.frontUrl, this.state.backUrl)
-      
+
     })
 
   }
@@ -43,7 +50,7 @@ class PokemonForm extends React.Component {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={this.handleSubmit}>
+        <Form onSubmit={(event) => this.handleSubmit(event)}>
           <Form.Group widths="equal">
             <Form.Input fluid label="Name" placeholder="Name" name="name" />
             <Form.Input fluid label="hp" placeholder="hp" name="hp" />
