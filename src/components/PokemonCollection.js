4646f04f2e6e1edfc4a0d29197f0d8001eb87d92
David Fellows <dfellows12@gmail.com>
2019-11-26 16:48:48 -0500

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,13 +3,22 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+  
+  renderPokemonCard = () => {
+    return this.props.pokemon.map(pokemon => {
+      return <PokemonCard pokemonInfo={pokemon}/>
+    })
+  }
+
   render() {
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        <h1>Pokemon Collection</h1>
+        {this.renderPokemonCard()}
       </Card.Group>
     )
   }
+
 }
 
 export default PokemonCollection
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -13,16 +13,51 @@ class PokemonForm extends React.Component {
     }
   }
 
+  onChangeHandler = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  onSubmitHandler = (event) => {
+    event.preventDefault()
+    let {name, hp, frontUrl, backUrl} = this.state
+    debugger
+    fetch('http://localhost:3000/pokemon', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        stats: [
+          {value: hp,
+          name: 'hp'
+        }
+        ],
+        sprites: {
+          front: frontUrl,
+          back: backUrl
+        }
+      })
+    })
+    .then(rsp => rsp.json())
+    .then(pokemonObj => {
+      this.props.addPokemon(pokemonObj)
+    })
+  }
+
+
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={this.handleSubmit}>
+        <Form onSubmit={(event) => this.onSubmitHandler(event)}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input onChange={(event) => this.onChangeHandler(event)} fluid label="Name" placeholder="Name" name="name" />
+            <Form.Input onChange={(event) => this.onChangeHandler(event)} fluid label="hp" placeholder="hp" name="hp" />
+            <Form.Input onChange={(event) => this.onChangeHandler(event)} fluid label="Front Image URL" placeholder="url" name="frontUrl" />
+            <Form.Input onChange={(event) => this.onChangeHandler(event)} fluid label="Back Image URL" placeholder="url" name="backUrl" />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
