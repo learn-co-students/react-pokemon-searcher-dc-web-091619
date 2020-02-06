b06999b8c0c08dfcef457e51736dd6b8f2a9f23d
Graham Flaspoehler <grahamflas@gmail.com>
2020-02-05 21:24:55 -0500

    Add new Pokemon

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -19,12 +19,28 @@ class PokemonPage extends React.Component {
   }
 
   fetchPokemon = () => {
+    console.log("fetching")
     fetch("http://localhost:3000/pokemon")
       .then(response => response.json())
       .then(pokemonArray => this.setState({
         allPokemon: pokemonArray
       }))
   }
+
+  addPokemon= data => {
+    
+    fetch("http://localhost:3000/pokemon", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(resp => resp.json())
+      .then(this.fetchPokemon())
+  }
+
+  
 
   filterPokemon = () => {
     const {allPokemon, searchTerm} = this.state
@@ -39,7 +55,7 @@ class PokemonPage extends React.Component {
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm addPokemon={this.addPokemon}/>
         <br />
         <Search onChange={this.handleSearchChange} searchTerm={this.state.searchTerm}/>
         <br />
